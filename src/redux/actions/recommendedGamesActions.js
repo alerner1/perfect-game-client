c1b0978812cf3b3270952c4c465f7815679028cc
alerner1 <alyssplerner@gmail.com>
2020-11-09 13:01:40 -0500

    advanced recs working but slow

--- src/redux/actions/recommendedGamesActions.js
+++ src/redux/actions/recommendedGamesActions.js
@@ -19,4 +19,26 @@ export function getQuickRecommendations() {
   }
 }
 
+export function getAdvancedRecommendations(formData) {
+  return (dispatch) => {
+    dispatch({ type: 'CLEAR_GAME' })
+    dispatch({ type: 'REQUESTING_RECS' })
+    const token = localStorage.getItem("token")
+    fetch('http://localhost:3000/api/v1/games/advanced_recommendations', {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Accepts": "application/json",
+        Authorization: `Bearer ${token}`
+      },
+      body: JSON.stringify(formData)
+    })
+    .then(resp => resp.json())
+    .then(json => {
+      dispatch({ type: 'ADD_RECOMMENDED_GAMES', payload: json })
+      dispatch({ type: 'ADD_SHOW_GAME', payload: json[0] })
+    })
+  }
+}
+
 export const requesting = () => ({ type: "REQUESTING_RECS" })
