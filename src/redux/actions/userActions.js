0815fe7e5c7c01da5f0d508c5f96c0f8c5b9503e
alerner1 <alyssaplerner@gmail.com>
2020-11-19 15:19:17 -0500

    trying to fix cors

--- src/redux/actions/userActions.js
+++ src/redux/actions/userActions.js
@@ -11,7 +11,6 @@ export function createUser(formData) {
       })
     })
       .then(response => {
-        debugger
         response.json()})
       .then(json => {
         localStorage.setItem("token", json.jwt);
