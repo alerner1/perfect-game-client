569fecfd6349b35e8de136cf4da24b0692d63f89
alerner1 <alyssaplerner@gmail.com>
2020-12-06 13:13:04 -0500

    redid app with hooks

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import React from 'react';
-import { connect } from 'react-redux';
-import { getUser, setUser } from './redux/actions/userActions';
-import { Route, Switch, withRouter } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { getUser } from './redux/actions/userActions';
+import { Route, Switch, useHistory } from 'react-router-dom';
 import NavMenuContainer from './NavMenu/containers/NavMenuContainer';
 import LoginForm from './Login/LoginForm';
 import SignupContainer from './Signup/containers/SignupContainer';
@@ -10,51 +10,36 @@ import GameListContainer from './GameLists/containers/GameListContainer';
 import RecommendationsContainer from './Recommendations/containers/RecommendationsContainer';
 import AdvancedRecsFormContainer from './Recommendations/containers/AdvancedRecsFormContainer';
 
-class App extends React.Component {
-  componentDidMount() {
+const App = () => {
+  const dispatch = useDispatch();
+  const history = useHistory();
+  
+  useEffect(() => {
     const token = localStorage.getItem("token");
     if (token) {
-      console.log("got a token");
-      this.props.getUser();
-      // this.props.history.push('/profile');
+      dispatch(getUser());
+      history.push('/game_lists/played_games');
     } else {
-      console.log('no token')
+      history.push('/login');
     }
-  }
+  }, []); 
 
-  render() {
-    return (
-      <div>
-        <NavMenuContainer />
-        <Switch>
-          <Route path="/login" render={() => <LoginForm />} />
-          <Route path="/signup" render={() => <SignupContainer />} />
-          {/* hypothetically we could have the component below show a "loading" thing until there is a currentUser. if currentUser is an empty obj or whatever then reroute? */}
-          <Route path="/game_lists/played_games" render={() => <ProfileContainer />} />
-          <Route path="/game_lists/owned_games" render={() => <GameListContainer list="owned" />} />
-          <Route path="/game_lists/wishlist" render={() => <GameListContainer list="wish" />} />
-          <Route path="/game_lists/saved_recommendations" render={() => <GameListContainer list="saved" />} />
-
-          <Route path="/quick_recommendations" render={() => <RecommendationsContainer />} />
-          <Route path="/advanced_recommendations/new" render={() => <AdvancedRecsFormContainer />} />
-          <Route path="/advanced_recommendations" render={() => <RecommendationsContainer />} />
-        </Switch>
-      </div>
-    )
-  }
+  return (
+    <div>
+      <NavMenuContainer />
+      <Switch>
+        <Route path="/login" render={() => <LoginForm />} />
+        <Route path="/signup" render={() => <SignupContainer />} />
+        <Route path="/game_lists/played_games" render={() => <ProfileContainer />} />
+        <Route path="/game_lists/owned_games" render={() => <GameListContainer list="owned" />} />
+        <Route path="/game_lists/wishlist" render={() => <GameListContainer list="wish" />} />
+        <Route path="/game_lists/saved_recommendations" render={() => <GameListContainer list="saved" />} />
+        <Route path="/quick_recommendations" render={() => <RecommendationsContainer />} />
+        <Route path="/advanced_recommendations/new" render={() => <AdvancedRecsFormContainer />} />
+        <Route path="/advanced_recommendations" render={() => <RecommendationsContainer />} />
+      </Switch>
+    </div>
+  )
 }
 
-const mapStateToProps = (state) => {
-  return {
-    currentUser: state.user.currentUser
-  }
-}
-
-const mapDispatchToProps = (dispatch) => {
-  return {
-    getUser: () => dispatch(getUser()),
-    setUser: () => dispatch(setUser())
-  };
-}
-
-export default connect(mapStateToProps, mapDispatchToProps)(withRouter(App));
+export default App;
